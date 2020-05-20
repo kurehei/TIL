43bcf7fe45470537a3d18d182f36aa53a36f0430
榑林晃平 <kureykou321@gmail.com>
2020-05-21 07:17:55 +0900

    配列の添字についての規約を学んだ

--- PHP_reference/array.php
+++ PHP_reference/array.php
@@ -45,6 +45,82 @@ $array3 = [
   6 => "c",
   "d"
 ];
-var_dump($array3);
+//var_dump($array3);
 
+// 配列の要素へのアクセス
+$array = array(
+  "foo" => "bar",
+    42    => 24,
+    "multiple" => array(
+      "dimensional" => array(
+        "array" => "foo"
+      )
+    )
+  );
+//var_dump($array["foo"]);
+//var_dump($array["multiple"]["dimensional"]["array"]);
+
+// 配列では、変数に代入しなくても直接、配列を扱える
+function getArray() {
+  return [1,2,3];
+}
+print getArray()[1].PHP_EOL;
+// 以前は
+$tmp = getArray();
+$tmp[1];
+
+//　配列の要素の追加、削除など
+$arr = [
+  5 => 1,
+  12 => 2
+];
+
+$arr["x"] = 42; // 配列にxというキーと、42という値を設定している
+
+unset($arr[5]); // ５という配列の要素を削除している
+unset($arr); // 配列全体を削除している
+
+$array4 = [1,2,3,4,5];
+print_r($array4);
+
+foreach($array4 as $i => $value) {
+  unset($array4[$i]);
+}
+print_r($array4);
+
+// 新しく要素を追加、ただし、添字は０ではなく5
+$array4[] = 6;
+print_r($array4);
+
+// リセットし直すには、array_values()を使う
+$array4 = array_values($array4);
+$array4[] = 7;
+print_r($array4);
+
+// 連想配列の場合、添字の場合、全て""で囲む
+$array = [
+  "foo" => "bar"
+];
+echo $array["foo"]; //$array[foo]ではだめ
+
+// エラー全文表示
+error_reporting(E_ALL);
+
+$arr2 = [
+  'fruit' => 'apple',
+  'veggie' => 'carrot'
+];
+// 正しいアクセス方法
+print ($arr2['fruit']).PHP_EOL;// apple
+
+//print_r($arr2[fruit]); // エラー
+
+define('fruit','veggie').PHP_EOL;// 定数、fruitの値にveggleをしよう
+print ($arr2['fruit']).PHP_EOL; // apple
+print ($arr2[fruit]).PHP_EOL; // "veggle"が呼ばれ、そこの配列の値であるcarrotが呼ばれる
+
+print "$arr2[fruit]".PHP_EOL; // 文字列ではエラーではなくなる
+print "{$arr2[fruit]}".PHP_EOL;
+
+// 連想配列の呼び出す際、""の引用符で囲んだ方が良い。囲まない場合、定数を呼び出しと期待した動きをするため
 ?>

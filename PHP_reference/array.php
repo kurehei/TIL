7b6694a570ed34a9f0c22cdb0ec22d939b3ba255
榑林晃平 <kureykou321@gmail.com>
2020-05-23 11:56:42 +0900

    first-commit

--- PHP_reference/array.php
+++ PHP_reference/array.php
@@ -123,4 +123,111 @@ print "$arr2[fruit]".PHP_EOL; // 文字列ではエラーではなくなる
 print "{$arr2[fruit]}".PHP_EOL;
 
 // 連想配列の呼び出す際、""の引用符で囲んだ方が良い。囲まない場合、定数を呼び出しと期待した動きをするため
+
+class A {
+  private $A;
+}
+
+class B extends A {
+  private $A; // これは '\0B\0A' となります
+  public $AA; // これは 'AA' となります
+}
+var_dump((array) new B());
+
+// 配列の色々な使用例
+$array5 = [
+  'color' => 'red',
+  'tasste' => 'sweet',
+  'shape' => 'round',
+  'name' => 'apple',
+  4 // キーは0になる
+];
+
+$array6 = [];
+
+$array6['color'] = 'red';
+$array6['taste'] = 'sweet';
+$array6['shape'] = 'round';
+$array6['name']  = 'apple';
+$array6[] = 4;
+
+var_dump($array6);
+print_r($result = array_diff($array6,$array5));
+// array_diffは配列の差を計算する
+
+$switting = [
+  10,
+  5 => 6,
+  3 => 7,
+  'a' => 4,
+  11 ,
+  '02' => 77,// keyは６
+  '8' => 2, // keyのString整数は、intにキャストされる
+  0 => 12 // 0 => 9になる
+];
+
+$switching2 = array(         10, // key = 0
+                    5    =>  6,
+                    3    =>  7,
+                    'a'  =>  4,
+                            11, // key = 6 (最大の添字は5です)
+                    '8'  =>  2, // key = 8 (整数!)
+                    '02' => 77, // key = '02'
+                    0    => 12  // 値10は12で上書きされます
+                  );
+var_dump($switching2);
+
+// 配列は、参照渡しする事で直接変更可能
+$colors = ['red', 'blue','yellow','green'];
+foreach($colors as $color) {
+  echo "Do you like $color\n";
+}
+
+// ループ内での要素の変更
+foreach ($colors as &$color) {
+  // foreachの中で直接値を変更したい場合、値に$をつける
+  $color = strtoupper($color);
+}
+unset($color); /* これ以降の $color への書き込みが
+配列の要素を書き換えてしまわないことを保証する */
+// unset()で参照を解除
+
+print_r($colors);// print_r() 指定した変数の情報をわかりやすく出力
+
+// 1から始まる添字
+$firstquarter  = array(1 => 'January', 'February', 'March');
+print_r($firstquarter);
+
+// 配列のソート
+sort($firstquarter);
+print_r($firstquarter);
+
+// 多次元配列
+$fruits = [
+  "fruits" => [
+                  "a" => "orange",
+                  "b" => "banana",
+                  "c" => "apple"
+                ],
+  "holes" =>[
+                  "first",
+                   5 => "second",
+                   "third"
+  ]
+];
+echo $fruits["holes"][5].PHP_EOL; // second
+echo $fruits["fruits"]["a"].PHP_EOL;
+// 多次元配列の追加
+$jucies["apple"]["green"] = "good";
+print_r($jucies);
+
+$arr = [2,3];
+$arr2 = $ar; //arr2が変更される
+$arr2[] = 4; //arrは変更されてない
+
+$arr3 = &$arr; //
+$arr3[] = 4 // arrとarr3は同じ内容
+
+// 参照による代入とは、両方の変数が同じデータを指すようにするということです。$ = &$が可能になる
+
 ?>

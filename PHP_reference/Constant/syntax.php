239b3973287220a026e634d61b1402200a91b095
榑林晃平 <kureykou321@gmail.com>
2020-06-10 00:18:41 +0900

    定数とマジック定数についてまなんだ

--- PHP_reference/Constant/syntax.php
+++ PHP_reference/Constant/syntax.php
@@ -18,6 +18,31 @@ echo aaa; // HELLO WORLDって出力される
 
 定数の名前を動的にしる場合、constant()で定数の値を知ることができる関数を使用することもできる。
 */
-var_dump(constant(CONSTANT)) ;
+// var_dump(constant(CONSTANT)) ;
+const ANIMALS = ["dog", "cat", "lion"];
+
+echo ANIMALS[0] . PHP_EOL; // PHP 5.6以降から配列での定数宣言ができるようになってきた
+//constはトップレベルで定数宣言をしなけれなならないため、関数、ループ文、if分、などで宣言できない。
+
+// マジカル定数宣
+//PHPには、実行されるスクリプト上で使用可能に多くの定義済み関数が存在する。使われる場所によって値が変わる定数（マジック定数）と呼ぶ。
+
+class Foo {
+  public function className() {
+    // クラス名を出力する
+    echo __CLASS__ .PHP_EOL;
+  }
+
+  // 静的メソッドで関数名を出力する
+  public static function methodName() {
+    echo __METHOD__.PHP_EOL;
+  }
+}
+
+$foo = new Foo();
+$foo->className();
+Foo::methodName();
+
+
 
 ?>
